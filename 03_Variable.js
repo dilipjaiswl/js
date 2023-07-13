f823ce925c482970341a7c43aa5eef501e29b4e1
Dilip Kumar Jaiswal <116095783+dilipjaiswl@users.noreply.github.com>
2023-07-14 00:16:35 +0530

    Add files via upload

---
+++ 03_Variable.js
@@ -0,0 +1,8 @@
+console.log("Hello World")
+ let num=4
+console.log(num) 
+console.log(num+3)
+let userName = "Dilip Jaiswal"  //camel casing rule imp
+console.log(userName)
+// user_name = "Dilip Jaiswal"  //snake casing rule
+//console.log(user_name)
